67ec0b9b172f404afb9dc7cd4f521b9da990cf42
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-09-29 10:30:52 -0700

    added some routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Navigate
+} from 'react-router-dom';
+import Layout from './Page/Layout.jsx';
+import Dashboard from './Dashboard/Dashboard.jsx';
+import Users from './Users/Users.jsx';
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element ={<Dashboard />}/>
+          <Route path="users" element={<Users />}/>
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
